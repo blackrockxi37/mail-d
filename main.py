ef0f22b7b4928b634cd1c16759d28ef8ce3a0b65
blackrockxi37 <agentsmith226@gmail.com>
2024-04-11 19:50:23 +0300

    some little changes

--- main.py
+++ main.py
@@ -9,43 +9,59 @@ import time
 import os
 
 
-chatid = -1001851749239
-#chatid = 316009566
-
+#tocken, group chat id and mine chat id
+chatid = -1001851749239 
+rockxi = 316009566
 token = '7186213219:AAFgSnkq1lDpUeyKVOZjl_PuRTScOWlaVhg'
-flag = True
-notyflag = False
-getMail_status = 0
-bot = tg.TeleBot(token)
-
 mail_pass = "3jG402P7cqVd587Fdv1L"
 username = "pi23-2b@mail.ru"
 imap_server = "imap.mail.ru"
-imap = imaplib.IMAP4_SSL(imap_server)
-imap.login(username, mail_pass)
 blacklist = ["<info@e.mail.ru>", "<security@id.mail.ru>" , "<robot@mlrmr.com>"]
 
+#some global flags
+flag = True
+notyflag = False
+getMail_status = 0
 
-def clean(text):
-    return "".join(c if c.isalnum() else "_" for c in text)
+#telegram bot and imap connection 
+bot = tg.TeleBot(token)
+imap = imaplib.IMAP4_SSL(imap_server)
+imap.login(username, mail_pass)
+f = open('times.txt', 'r+')
 
 
+#main function that check email and send last mail to tg group
+#
+#try - except construction is using for catch logout exception
+#
+#
 def getMail():
     flagy = False
-    a = imap.select('INBOX')[1][0]
-    a = int(a) - 0
-    a = str(a)
+    global getMail_status
+    
+
+    try:
+        a = imap.select('INBOX')[1][0]
+    except Exception as e:
+        getMail_status = 1
+        bot.send_message(rockxi, str(e))
+        imap.login(username, mail_pass)
+        a = imap.select('INBOX')[1][0]
+    
+    a = str(int(a) - 0)
     res, msg = imap.fetch(a, '(RFC822)')
     msg = email.message_from_bytes(msg[0][1])
-    payload=msg.get_payload()
-    f = open('times.txt', 'r+')
     subject, encoding = decode_header(msg["Subject"])[0]
     if isinstance(subject, bytes):
         subject = subject.decode(encoding)
+
     letter_from = msg["Return-path"]
+    # blacklist email's check
     for b in blacklist:
         if str(letter_from) == b:
+            # banned robot mail bitch !_!
             return "Уведомление от mail.ru."
+    
     for part in msg.walk():
         content_disposition = str(part.get("Content-Disposition"))
         if part.get_content_maintype() == 'text' and part.get_content_subtype() == 'plain':
@@ -53,13 +69,16 @@ def getMail():
             times = f.readlines()
             time = email.utils.parsedate_tz(msg["Date"])
             messagetime = datetime(time[0],time[1],time[2],time[3],time[4],time[5])
+
             if str(messagetime) + '\n' not in times:
                 flagy = True
                 f.write(str(messagetime) + '\n')
                 sendMail(string)
             else:
+                #funny code >_<
                 return "Нет новых сообщений!"
             
+
         if "attachment" in content_disposition:
             times = f.readlines()
             if flagy:
@@ -74,14 +93,13 @@ def getMail():
                     filepath = os.path.join(folder_name, filename)
                     open(filepath, "wb").write(part.get_payload(decode=True))
                     sendMail('', filepath)
-    
+
+    #fine code =)
     return 'Сообщение отработано!'   
 
 
 def sendMail(mail, file= False):
     if not file:
-        #sanya = '<b><a href = "https://t.me/Sabelnikov_A">Саня</a>, хуй соси!</b>\n\n'
-
         bot.send_message(chat_id = chatid, text = mail)
         print('отправляю сообщение...')
     if file:
@@ -100,8 +118,8 @@ def ThreadMailReader():
             a = getMail()
             print(f'<{count}> --> , {a}')
         except Exception as e:
-            bot.send_message(chat_id= 316009566, text = str(e), disable_notification=notyflag)
-            notyflag = False
+            bot.send_message(chat_id=rockxi, text = str(e), disable_notification=notyflag)
+            notyflag = True
             getMail_status = 1
         time.sleep(60)
         
@@ -110,8 +128,8 @@ def ThreadMailReader():
 @bot.message_handler()
 def messahe_handler(message):
     global getMail_status
-    if message.chat.id == 316009566:
-        bot.send_message(316009566, f"Работает. getMail статус = {getMail_status}")
+    if message.chat.id == rockxi:
+        bot.send_message(rockxi, f"Работает.\ngetMail()_status = {getMail_status}")
         print(message.from_user.username, ", ", message.chat.id, " : ", message.text.strip())
 
 t1 = threading.Thread(target=ThreadMailReader, daemon=True)
@@ -119,7 +137,7 @@ t1.start()
 
 
 try:
-    ans = print("Нажмите Ctrl + С чтобы выйти.\n")
+    ans = print("Нажмите Ctrl + С чтобы выйти.")
     bot.polling()
 except KeyboardInterrupt:
     print("Ctrl + С")
