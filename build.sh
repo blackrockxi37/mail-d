92b8d630014ceb3fc8fef11fc3ae0a774c49fc02
blackrockxi37 <agentsmith226@gmail.com>
2024-09-12 23:55:49 +0300

    fix

--- build.sh
+++ build.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 echo "Установите предварительно docker"
-read -p "Введите IMAP сервер: " imap_server
 read -p "Введите почтовый ящик: " username
 read -p "Введите пароль от IMAP: " mail_pass
+read -p "Введите IMAP сервер: " imap_server
 echo 
 
-read -p "Введите chatid: " chatid
-read -p "Введите adminid: " rockxi
-read -p "Введите token от TG бота: " token
+read -p "Введите chat id (чат группы, куда будут приходить письма): " chatid
+read -p "Введите id администратора бота (свой личный): " rockxi
+read -p "Введите token от TG бота (выдаёт botFather): " token
 
 cat <<EOL > hiddendata.py
-imap_server = "$imap_server"
 username = "$username"
 mail_pass = "$mail_pass"
+imap_server = "$imap_server"
 chatid = "$chatid"
 rockxi = "$rockxi"
 token = "$token"
-blacklist = [ "<info@e.mail.ru>", "<security@id.mail.ru>" , "<robot@mlrmr.com>" ]
+blacklist = [ "<info@e.mail.ru>", "<security@id.mail.ru>" , "<robot@mlrmr.com>" ] 
+# blacklist, письма из ящиков, содержащихся в этом массиве не будут пересылаться (не забудь про <> вокруг ящика)
 EOL
 
-echo "Данные успешно записаны в hiddendata.py. Теперь соберите docker - контейнер с помощью:
+echo "Данные успешно записаны в hiddendata.py. Теперь соберите docker - контейнер с помощью\n:
         docker build -t mail-d
         И запустите с помощью:
-        docker run -d --restart always mail-d\n"
+        docker run -d --restart always mail-d\n
+        Чтобы посмотреть ошибки, запусти контейнер без ключа -d"
