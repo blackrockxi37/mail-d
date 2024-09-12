01d99dff471b542fbe70824ea80e9b7f8f8b3f6e
blackrockxi37 <agentsmith226@gmail.com>
2024-09-12 23:52:59 +0300

    fix

--- build.sh
+++ build.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 echo "Установите предварительно docker"
 read -p "Введите IMAP сервер: " imap_server
-read -p "Введите почтовый ящик: " email
-read -p "Введите пароль от IMAP: " imap_password
+read -p "Введите почтовый ящик: " username
+read -p "Введите пароль от IMAP: " mail_pass
 echo 
 
 read -p "Введите chatid: " chatid
 read -p "Введите adminid: " rockxi
-read -p "Введите token от TG бота: " tg_token
+read -p "Введите token от TG бота: " token
 
 cat <<EOL > hiddendata.py
 imap_server = "$imap_server"
-username = "$email"
-mail_pass = "$imap_password"
+username = "$username"
+mail_pass = "$mail_pass"
 chatid = "$chatid"
-rockxi = "$adminid"
-token = "$tg_token"
+rockxi = "$rockxi"
+token = "$token"
 blacklist = [ "<info@e.mail.ru>", "<security@id.mail.ru>" , "<robot@mlrmr.com>" ]
 EOL
 
-echo "Данные успешно записаны в hiddendata.py. Теперь соберите контейнер с помощью:\n
-        docker build -t mail-d\n
-        docker run -d --restart always mail-d"
+echo "Данные успешно записаны в hiddendata.py. Теперь соберите docker - контейнер с помощью:
+        docker build -t mail-d
+        И запустите с помощью:
+        docker run -d --restart always mail-d\n"
